ee8c727aa263630e06f7f142b2769701e238d93d
hermitex <mercytibs@yahoo.com>
2022-08-31 16:10:41 +0300

    add car model test

--- server/spec/models/cars/car_spec.rb
+++ server/spec/models/cars/car_spec.rb
@@ -1,3 +1,20 @@
 describe Car do
-   
+    let(:car) {Car.new}
+    let(:attributes) do
+        {
+            VIN: 1234,
+            year: 2000
+            name: "Car",
+            model: "Some Model",
+            price: 2000,
+            milage: 1000000,
+            body_style: "Regular",
+            color: "brown"
+            description: "Some Description",   
+            is_sold: false                     
+        }
+    end
+    it 'inherits from ActiveRecord::Base' do
+        expect(Car,superclass).to   eq(ActiveRecord::Base)
+    end
 end
