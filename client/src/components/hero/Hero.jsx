e97c6a69c69ddcd7b5700f84c3a308c63f02c562
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-09-04 14:45:23 +0300

    fix slider controlls

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -99,6 +99,7 @@ function Hero() {
             <button
               type="button"
               onClick={handleClick}
+              id="prev"
               className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 rotate-180"
             >
               <svg
@@ -119,6 +120,7 @@ function Hero() {
             <button
               type="button"
               onClick={handleClick}
+              id="next"
               className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
             >
               <svg
